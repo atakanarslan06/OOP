0e316e74b69859d83db6440bd1ad9a18bb5ae55b
atakanarslan06 <johndoe@example.com>
2023-02-16 12:42:45 +0300

    OOP Field

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Class Members
+        #region Field
+        //Nesne içerisinde değer tutmamızı sağlayan alanlardır
+        MyClass m1 = new MyClass(); //Nesne Oluşturduk!!!
+        m1.a = 1;
+        MyClass m2 = new MyClass(); //Nesne Oluşturduk!!!
+        m2.a = 2;
+
+        #endregion
+        #endregion
+    }
+}
+
+class MyClass
+{
+    public int a;
+    public string b;
+}
